70cc9a0273da67ce00476782bdcc7b9406324a88
hsejsx <hsejsx@gmail.com>
2023-05-30 19:30:45 +0900

    Study 마크업 및 스타일

--- src/pages/Study.jsx
+++ src/pages/Study.jsx
@@ -10,32 +10,32 @@ export default function Study() {
     if (cachedData) {
       setCountries(JSON.parse(cachedData));
     } else {
-    fetch(
-      `https://apis.data.go.kr/1262000/CountryBasicService/getCountryBasicList?serviceKey=${
-        import.meta.env.VITE_APP_SERVICE_KEY
+      fetch(
+        `https://apis.data.go.kr/1262000/CountryBasicService/getCountryBasicList?serviceKey=${
+          import.meta.env.VITE_APP_SERVICE_KEY
         }&numOfRows=200`
-    )
-      .then(res => res.text())
-      .then(data => {
-        let xml = new XMLParser().parseFromString(data);
-        const items = xml.children[1].children[0].children;
-        const extractedData = items.map(item => ({
-          capital: item.children
-            .find(child => child.name === 'basic')
-            .value.match(regex)
-            ? item.children
-                .find(child => child.name === 'basic')
-                .value.match(regex)[1]
-            : '',
+      )
+        .then(res => res.text())
+        .then(data => {
+          let xml = new XMLParser().parseFromString(data);
+          const items = xml.children[1].children[0].children;
+          const extractedData = items.map(item => ({
+            capital: item.children
+              .find(child => child.name === 'basic')
+              .value.match(regex)
+              ? item.children
+                  .find(child => child.name === 'basic')
+                  .value.match(regex)[1]
+              : '',
             countryName: item.children.find(
               child => child.name === 'countryName'
             ).value,
-          imgUrl: item.children
-            .find(child => child.name === 'imgUrl')
-            .value.replace('&amp;', '&'),
-        }));
-        console.log(extractedData);
-        setCountries(extractedData);
+            imgUrl: item.children
+              .find(child => child.name === 'imgUrl')
+              .value.replace('&amp;', '&'),
+          }));
+          console.log(extractedData);
+          setCountries(extractedData);
           setCountries(prev => {
             localStorage.setItem(
               'countriesData',
@@ -43,9 +43,41 @@ export default function Study() {
             );
             return prev.filter(country => country.capital);
           });
-      })
-      .catch(err => console.log(err));
+        })
+        .catch(err => console.log(err));
     }
   }, []);
-  return <div></div>;
+
+  if (countries)
+    return (
+      <section className='flex flex-col w-full mx-auto max-w-xl'>
+        {countries.map((country, index) => (
+          <section key={index} className='md:flex items-start mb-4'>
+            <div className='w-full h-auto max-w-xl md:flex-shrink-0 md:basis-[40%] mb-2'>
+              <img
+                className='w-full h-full object-cover'
+                src={country.imgUrl}
+                alt=''
+              />
+            </div>
+            <table className='table-auto flex-grow text-center'>
+              <thead>
+                <tr>
+                  <th className='px-1 py-2'>번호</th>
+                  <th className='px-4 py-2'>나라</th>
+                  <th className='px-4 py-2'>수도</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td className='border px-4 py-2'>{index + 1}</td>
+                  <td className='border px-4 py-2'>{country.countryName}</td>
+                  <td className='border px-4 py-2'>{country.capital}</td>
+                </tr>
+              </tbody>
+            </table>
+          </section>
+        ))}
+      </section>
+    );
 }
